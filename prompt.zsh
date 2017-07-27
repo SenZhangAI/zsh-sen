7d6bff5fc939e61d45e6cff885cea39dd2cd0169
Sen Zhang <szhang.hust@gmail.com>
2017-07-27 09:26:48 +0800

    change prompt style

--- prompt.zsh
+++ prompt.zsh
@@ -1,32 +1,8 @@
 # cheers, @ehrenmurdick
 # http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
 
-autoload colors && colors
-
-if (( $+commands[git] ))
-then
-  git="$commands[git]"
-else
-  git="/usr/bin/git"
-fi
-
-git_branch() {
-  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
-}
-
-git_dirty() {
-  if $(! $git status -s &> /dev/null)
-  then
-    echo ""
-  else
-    if [[ $($git status --porcelain) == "" ]]
-    then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
-    else
-      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
-    fi
-  fi
-}
+autoload -U colors && colors
+setopt prompt_subst
 
 # VCS
 YS_VCS_PROMPT_PREFIX1=" %{$fg[white]%}on%{$reset_color%} "
@@ -40,58 +16,31 @@ ZSH_THEME_GIT_PROMPT_SUFFIX="$YS_VCS_PROMPT_SUFFIX"
 ZSH_THEME_GIT_PROMPT_DIRTY="$YS_VCS_PROMPT_DIRTY"
 ZSH_THEME_GIT_PROMPT_CLEAN="$YS_VCS_PROMPT_CLEAN"
 
+function git_prompt_info() {
+  local ref
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+  echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+}
+
 # Checks if working tree is dirty
 function parse_git_dirty() {
   local STATUS=''
   local FLAGS
   FLAGS=('--porcelain')
   FLAGS+='--ignore-submodules=dirty'
-  STATUS=$(time command git status ${FLAGS} 2> /dev/null | tail -n1)
+  STATUS=$(git status ${FLAGS} 2> /dev/null | tail -n1)
 
   if [[ -n $STATUS ]]; then
-    echo " %{$fg[red]%}x"
+    echo "$ZSH_THEME_GIT_PROMPT_DIRTY"
   else
-    echo " %{$fg[green]%}o"
+    echo "$ZSH_THEME_GIT_PROMPT_CLEAN"
   fi
 }
-
-function git_prompt_info() {
-  local ref
-  ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
-    ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
-  echo $ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX
-}
-
-#function git_prompt_info () {
-#ref=$($git symbolic-ref HEAD 2>/dev/null) || return
-## echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
-#echo "${ref#refs/heads/}"
-#}
 
 # Git info
-#local git_info=$(git_prompt_info)
+#local git_info='$(git_prompt_info)'
 local git_info=''
-
-# This assumes that you always have an origin named `origin`, and that you only
-# care about one specific origin. If this is not the case, you might want to use
-# `$git cherry -v @{upstream}` instead.
-need_push () {
-  if [ $($git rev-parse --is-inside-work-tree 2>/dev/null) ]
-  then
-    number=$($git cherry -v origin/$(git symbolic-ref --short HEAD) 2>/dev/null | wc -l | bc)
-
-    if [[ $number == 0 ]]
-    then
-      echo " "
-    else
-      echo " with %{$fg_bold[magenta]%}$number unpushed%{$reset_color%}"
-    fi
-  fi
-}
-
-directory_name() {
-  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
-}
+local exit_code="%(?,,C:%{$fg[red]%}%?%{$reset_color%})"
 
 PROMPT="
 %{$terminfo[bold]$fg[blue]%}#%{$reset_color%} \
@@ -100,36 +49,6 @@ PROMPT="
 %{$fg[green]%}%m \
 %{$fg[white]%}in \
 %{$terminfo[bold]$fg[yellow]%}%~%{$reset_color%}\
-${git_info}\
- \
+${git_info} \
 %{$fg[white]%}[%*] $exit_code
 %{$terminfo[bold]$fg[red]%}$ %{$reset_color%}"
-
-#export PROMPT="\n%{$(battery_status)%}in $(directory_name) $(git_dirty)$(need_push)\n›"
-set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
-}
-
-precmd() {
-  title "zsh" "%m" "%55<...<%~"
-  set_prompt
-}
-
-# From http://dotfiles.org/~_why/.zshrc
-# Sets the window title nicely no matter where you are
-function title() {
-  # escape '%' chars in $1, make nonprintables visible
-  a=${(V)1//\%/\%\%}
-
-  # Truncate command, and join lines.
-  a=$(print -Pn "%40>...>$a" | tr -d "\n")
-
-  case $TERM in
-    screen)
-      print -Pn "\ek$a:$3\e\\" # screen title (in ^A")
-      ;;
-    xterm*|rxvt)
-      print -Pn "\e]2;$2\a" # plain xterm title ($3 for pwd)
-      ;;
-  esac
-}
