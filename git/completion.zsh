f4aa25088e4537ea550b33b75e64178d07e57ca0
Sen Zhang <szhang.hust@gmail.com>
2017-07-27 09:27:27 +0800

    delete useless part

--- git/completion.zsh
+++ git/completion.zsh
@@ -2,9 +2,3 @@
 # bash `git-completion` script at $completion below (this is where Homebrew
 # tosses it, at least).
 #
-completion='usr/share/zsh/'$ZSH_VERSION'/site-functions/_git'
-
-if test -f $completion
-then
-  source $completion
-fi
